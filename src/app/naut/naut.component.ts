4ec6987c752fb4823cb9276a7676361550fdbd82
Jeff Sallans <jeffsallans@gmail.com>
2020-04-16 20:31:43 -0400

    I am happy with the result

--- src/app/naut/naut.component.ts
+++ src/app/naut/naut.component.ts
@@ -2,12 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 enum NautState {
   Unselected = 0,
-  RedSelected,
-  RedBanned,
-  BlueSelected,
-  BlueBanned,
-  BothSelected,
-  BothBanned,
+  Selected,
+  Banned,
 }
 
 @Component({
@@ -19,7 +15,8 @@ export class NautComponent implements OnInit {
   @Input() name: string = '';
   @Input() src: string = '';
 
-  state = NautState.Unselected;
+  redState = NautState.Unselected;
+  blueState = NautState.Unselected;
   NautState = NautState;
 
   constructor() { }
@@ -27,8 +24,15 @@ export class NautComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  toggleNaut(): void {
-    console.log(this.state);
-    this.state = (this.state + 1) % (NautState.BothBanned + 1);
+  toggleRedNaut(): void {
+    console.log(this.redState);
+    this.redState = (this.redState + 1) % (NautState.Banned + 1);
+  }
+
+  /** Return false to stop the propagation to context menu click */
+  toggleBlueNaut(): boolean {
+    console.log(this.blueState);
+    this.blueState = (this.blueState + 1) % (NautState.Banned + 1);
+    return false;
   }
 }
